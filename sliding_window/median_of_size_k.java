4d96ef388afb0a10ba8ba988b368e3093fe3ace7
nuglifeleoji <Leo3527703410@outlook.com>
2025-12-20 14:13:57 +0800

    shrexde

---
+++ sliding_window/median_of_size_k.java
@@ -0,0 +1,11 @@
+package sliding_window;
+
+import java.util.TreeSet;
+
+public class median_of_size_k{
+    public double[] MedianOfSizeK(int[] nums, int k){
+        double[] result=new double[nums.length-k+1];
+        TreeSet<Integer> smaller=new TreeSet<>();
+        
+    }
+}
